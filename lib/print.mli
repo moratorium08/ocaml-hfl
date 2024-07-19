a3b8b05a16dd927251937191d4639da485c7d841
Ken Sakayori <kensakayori+research@gmail.com>
2024-07-19 20:37:38 +0900

    doc(print): polish the document

--- lib/print.mli
+++ lib/print.mli
@@ -1,15 +1,16 @@
 (** Module for printing which extends [Stdlib.Format] *)
 
-(** ['a Print.t] is simply a type for the formatter of type ['a]. *)
-(** That is, ['a Print.t = 'a t = Format.formatter -> 'a -> unit ] *)
+(** ['a Print.t] is simply a type for the formatter of type ['a].
+    That is, ['a Print.t = 'a t = Format.formatter -> 'a -> unit ] *)
 (** This module includes [Fmt] from the fmt library *)
 
+(** {1 [Stdlib.Format] and [Fmt]} *)
 
 include module type of Format
 include module type of Fmt
 
 
-(** Utility operators and functions *)
+(** {2 Additional utility operators and functions} *)
 
 val ( ^^ ) :
   ('a, 'b, 'c, 'd, 'e, 'f) format6 ->
@@ -20,9 +21,13 @@ val list_comma : 'a t -> 'a list t
 val list_semi : 'a t -> 'a list t
 val list_set : 'a t -> 'a list t
 
-(** HFL specific formatter starts here *)
+
+(** {1 Formatters for HFL} *)
+
 (** In the following, a function [foo_] is defined as [foo] with operator precendence *)
 (* Do we realy want [foo_] to be exposed? *)
+
+(** {2 Operator Precendece}*)
 
 module Prec :
   sig
@@ -48,8 +53,12 @@ module Prec :
   end
 
 type prec = Prec.t
-type 'a t_with_prec = Prec.t -> 'a t
+type 'a t_with_prec = prec -> 'a t
 val ignore_prec : 'a t -> 'a t_with_prec
+
+
+(** {2 Formatter} *)
+
 val show_paren :
   bool -> formatter -> ('a, formatter, unit) Base.format -> 'a
 
@@ -62,7 +71,7 @@ val op_ : Arith.op t_with_prec
 val gen_arith : 'avar t_with_prec -> 'avar Arith.gen_t t
 val gen_arith_ : 'avar t_with_prec -> 'avar Arith.gen_t t_with_prec
 val arith : Arith.t t
-val arith_ : Prec.t -> Arith.t t
+val arith_ : prec -> Arith.t t
 val pred : Formula.pred t
 val pred_ : Formula.pred t_with_prec
 val gen_formula :
@@ -74,15 +83,15 @@ val gen_formula_ :
 val formula : Formula.t t
 val formula_ : Formula.t t_with_prec
 val argty : 'ty t -> 'ty Type.arg t
-val argty_ : (Prec.t -> 'ty t) -> Prec.t -> 'ty Type.arg t
+val argty_ : (prec -> 'ty t) -> prec -> 'ty Type.arg t
 val ty : ?with_var:bool -> 'annot t -> 'annot Type.ty t
-val ty_ : ?with_var:bool -> 'annot t -> Prec.t -> 'annot Type.ty t
+val ty_ : ?with_var:bool -> 'annot t -> prec -> 'annot Type.ty t
 val simple_ty : Type.simple_ty t
-val simple_ty_ : Prec.t -> Type.simple_ty t
+val simple_ty_ : prec -> Type.simple_ty t
 val simple_argty : Type.simple_ty Type.arg t
-val simple_argty_ : Prec.t -> Type.simple_ty Type.arg t
+val simple_argty_ : prec -> Type.simple_ty Type.arg t
 val fixpoint : Fixpoint.t t
-val hflz : (Prec.t -> 'ty t) -> 'ty Hflz.t t
-val hflz_ : (Prec.t -> 'ty t) -> Prec.t -> 'ty Hflz.t t
-val hflz_hes_rule : (Prec.t -> 'ty t) -> 'ty Hflz.hes_rule t
-val hflz_hes : (Prec.t -> 'ty t) -> 'ty Hflz.hes t
+val hflz : (prec -> 'ty t) -> 'ty Hflz.t t
+val hflz_ : (prec -> 'ty t) -> prec -> 'ty Hflz.t t
+val hflz_hes_rule : (prec -> 'ty t) -> 'ty Hflz.hes_rule t
+val hflz_hes : (prec -> 'ty t) -> 'ty Hflz.hes t
