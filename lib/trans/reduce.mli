5a10669956750e074ffacf5657406c6a899ef0ba
Ken Sakayori <kensakayori+research@gmail.com>
2024-07-26 22:29:18 +0900

    feat (trans/reduce): add an interface file

---
+++ lib/trans/reduce.mli
@@ -0,0 +1,14 @@
+(** Module for reducing formulas *)
+
+module Hflz :
+  sig
+
+    (** Beta-reduces redexes of a body formula.
+        Predicate variables are not are not unfolded by this function
+        (and thus additional redexes won't be introduced). *)
+    val beta : 'a Hflz.t -> 'a Hflz.t
+
+    (* TODO: document that it checks whether predicates are mutually recursivevly defined *)
+    (** Inlines predicate variables *)
+    val inline : Type.simple_ty Hflz.hes -> Type.simple_ty Hflz.hes
+  end
